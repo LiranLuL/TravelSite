8d5d26eef8a85d227cfcedece8ca89695a3b77b1
LiranLUL <kalitin.alecsey.01@gmail.com>
2020-06-14 16:38:04 +0300

    fix

--- script.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function showHeader() {
-    var header = document.querySelector('.navbar');
-    if (window.pageYOffset > 50) {
-        header.classList.add('header_fixed');
-    } else {
-        header.classList.remove('header_fixed');
-    }
-}
